a4fd41a030fd53d683c8a6a0ff96f4f68fb30c0e
ianburrows04 <ianburrows777@gmail.com>
2024-02-08 14:35:48 -0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,36 @@
 import random
-num = random.randint(1, 100)
 
-user = input("Enter your guess: ")
-user = int(user)
+play_again = 1
 
-if user > num:
-    print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
+while play_again == 1:
+    num = random.randint(1, 100)
+
+    guess = 0
+    user = 0
+    bottom = 1
+    top = 100
+
+    while user != num:
+        guess = guess + 1
+        print("Guess a number between", bottom, "and", top)
+        user = input()
+        user = int(user)
+
+        if user > num:
+            print("Too high")
+            top = user
+        elif user < num:
+            print("Too low")
+            bottom = user
+        else:
+            print("You got it!")
+            print("You guessed", guess, "times")
+    
+    play_again = input("Do you want to play again? Type 1 for yes and 2 for no: ")
+    play_again = int(play_again)
+            
+    if play_again == 2:
+        print("Goodbye.")
+        break
+    else:
+        continue
